b8123ffed65c21b3ef8e6a6ecdd45ff44fad32d8
Fako_Samir <coffinho@github.com>
2021-12-29 16:34:44 +0100

    feature | Skills container
    ----------
     MAIN
     * Created navbar
     * Created Hero container
    ----------
     NOTE
     * I'm still working on skills container
    ----------

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/NavbarContainer";
 import Hero from "./components/HeroContainer";
 import About from "./components/AboutContainer";
+import Skills from "./components/SkillsContainer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }

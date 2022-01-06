c4bffa956d9620e3348aa9c5e3af2cde410141b3
Fako_Samir <coffinho@github.com>
2022-01-06 16:43:48 +0100

    Ticket Number | Main Title 06.01.2022 ----------  MAIN  * SkillsContainer ----------  NOTE  * Working on Skills section, problems with data by importing mobile data. ----------

--- src/components/SkillsContainer/index.js
+++ src/components/SkillsContainer/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import data from "./skillsData";
+import dataMobile from "./skillsDataMobile";
 import "./skills.scss";
 const Skills = () => {
   return (
     <div className="skills-section">
-      <h1>
+      <h1 className="skills-title">
         Our skill set <span className="skills-line"></span>
         <span className="skills-grey-text">What we actually do</span>
       </h1>
@@ -15,6 +16,18 @@ const Skills = () => {
             <div className="card-text">
               <h2>{item.title}</h2>
               <p> {item.description}</p>
+            </div>
+          </article>
+        ))}
+      </section>
+      <section class="cards-mobile">
+        {dataMobile.map((item) => (
+          <article class="card-mobile">
+            <img src={item.image1} alt="image" />
+            <img src={item.image2} alt="image" />
+            <div className="card-text-mobile">
+              <h2 className="title-mobile">{item.title}</h2>
+              <p className="content-mobile"> {item.description}</p>
             </div>
           </article>
         ))}
---
+++ src/components/SkillsContainer/skillsDataMobile.js
@@ -0,0 +1,49 @@
+import skillsImg from "../../assets/images/space-colony-website-illustrations-01@3x.png";
+import skillsImg1 from "../../assets/images/space-colony-website-illustrations-05@3x.png";
+import skillsImg2 from "../../assets/images/space-colony-website-illustrations-06@3x.png";
+import skillsImg3 from "../../assets/images/space-colony-website-illustrations-07@3x.png";
+import skillsImg4 from "../../assets/images/space-colony-website-illustrations-08@3x.png";
+import skillsImg5 from "../../assets/images/space-colony-website-illustrations-09@3x.png";
+import skillsImg6 from "../../assets/images/space-colony-website-illustrations-10@3x.png";
+import skillsImg7 from "../../assets/images/space-colony-website-illustrations-11@3x.png";
+
+const dataMobile = [
+  {
+    id: 1,
+    image1: skillsImg,
+    image2: skillsImg2,
+    title: "Planning and Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur",
+  },
+  {
+    id: 2,
+    image1: skillsImg1,
+    title: "UIUX design",
+    description: "Lorem ipsum dolor sit amet, consectetur",
+  },
+  {
+    id: 3,
+    image1: skillsImg3,
+    image2: skillsImg4,
+    title: "Android apps and iOS apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur",
+  },
+  {
+    id: 4,
+    image1: skillsImg5,
+    title: "Websites",
+    description: "Lorem ipsum dolor sit amet, consectetur",
+  },
+  {
+    id: 5,
+    image1: skillsImg6,
+    image2: skillsImg7,
+    title: "Startups and Learnio",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur",
+  },
+];
+
+export default dataMobile;
